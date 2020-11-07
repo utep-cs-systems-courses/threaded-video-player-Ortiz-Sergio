152c9427ef245b06c471fbe3274fc4f523f293dd
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-11-07 06:36:14 -0700

    first draft of extract frames

--- videoPlayer.py
+++ videoPlayer.py
@@ -1,3 +1,19 @@
 #! /usr/bin/env python3
 
-print("setting up privileges")
+import cv2
+
+class ProducerConsumer:
+
+    def extract_frames(userFile, queue):
+        count = 0;
+        video_file = cv2.VideoCapture(userFile)
+
+        success, image = video_file.read()
+
+        while success:
+            queue.put(image)
+
+            success, image = video_file.read()
+            count += 1
+
+        queue.put('\U0001F920')
