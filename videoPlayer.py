244459c188f65f179f39094218a3ffd53cbf0b31
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-11-07 06:41:16 -0700

    first draft convert grayscale method

--- videoPlayer.py
+++ videoPlayer.py
@@ -5,7 +5,7 @@ import cv2
 class ProducerConsumer:
 
     def extract_frames(userFile, queue):
-        count = 0;
+        count = 0
         video_file = cv2.VideoCapture(userFile)
 
         success, image = video_file.read()
@@ -17,3 +17,20 @@ class ProducerConsumer:
             count += 1
 
         queue.put('\U0001F920')
+
+    def convert_grayscale(color, gray):
+        count = 0
+
+        colorFrame = color.get()
+
+        while colorFrame is not '\U0001F920':
+            grayFrame = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+
+            gray.put(grayFrame)
+            count += 1
+
+            colorFrame = color.get()
+
+        gray.put('\U0001F920')
+
+        
