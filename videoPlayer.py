c9fb74a99dcabf99ce80ccdd68ec324c74cfe14d
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-11-07 19:01:35 -0700

    finalized producer consumer queue

--- videoPlayer.py
+++ videoPlayer.py
@@ -1,36 +1,81 @@
 #! /usr/bin/env python3
 
 import cv2
+import threading
 
 class ProducerConsumer:
+    def __init__(self):
+        self.queue = []
+        self.lock = threading.Lock()
+        self.full = threading.Semaphore(0)
+        self.empty = threading.Semaphore(24)
 
-    def extract_frames(userFile, queue):
-        count = 0
-        video_file = cv2.VideoCapture(userFile)
+    def put(self, frame):
+        self.empty.acquire()
+        self.lock.acquire()
+        self.queue.append(frame)
+        self.lock.release()
+        self.full.release()
+
+    def get(self):
+        self.full.acquire()
+        self.lock.acquire()
+        frame = self.queue.pop(0)
+        self.lock.release()
+        self.empty.release()
+        return frame
+
+def extract_frames(userFile, queue):
+    count = 0
+    video_file = cv2.VideoCapture(userFile)
+
+    success, image = video_file.read()
+
+    while success:
+        queue.put(image)
 
         success, image = video_file.read()
+        count += 1
 
-        while success:
-            queue.put(image)
+    queue.put('end')
 
-            success, image = video_file.read()
-            count += 1
+def convert_grayscale(color, gray):
+    count = 0
 
-        queue.put('\U0001F920')
+    colorFrame = color.get()
 
-    def convert_grayscale(color, gray):
-        count = 0
+    while colorFrame is not 'end':
+        grayFrame = cv2.cvtColor(colorFrame, cv2.COLOR_BGR2GRAY)
+
+        gray.put(grayFrame)
+        count += 1
 
         colorFrame = color.get()
 
-        while colorFrame is not '\U0001F920':
-            grayFrame = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+    gray.put('end')
 
-            gray.put(grayFrame)
-            count += 1
+def display_frames(frames):
+    count = 0
+    currentFrame = frames.get()
 
-            colorFrame = color.get()
+    while currentFrame is not 'end':
+        cv2.imshow('Video', currentFrame)
+        if cv2.waitKey(42) and 0xFF == ord("q"):
+            break
+        count += 1
 
-        gray.put('\U0001F920')
+        currentFrame = frames.get()
+
+    cv2.destroyAllWindows()
 
         
+color_frames = ProducerConsumer()
+gray_frames = ProducerConsumer()
+
+extract = threading.Thread(target = extract_frames, args = ('clip.mp4', color_frames))
+convert = threading.Thread(target = convert_grayscale, args = (color_frames, gray_frames))
+display = threading.Thread(target = display_frames, args = (gray_frames,))
+
+extract.start()
+convert.start()
+display.start()
